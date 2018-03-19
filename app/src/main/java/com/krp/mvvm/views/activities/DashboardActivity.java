92e1f1d89bee06575c8d6f482cb3ab89e31f6256
Rakesh Konduru <rakeshpraneeth@gmail.com>
2018-03-18 23:56:24 -0400

    * Created a View for Dashboard screen.

---
+++ app/src/main/java/com/krp/mvvm/views/activities/DashboardActivity.java
@@ -0,0 +1,15 @@
+package com.krp.mvvm.views.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.krp.mvvm.R;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
--- app/src/main/java/com/krp/mvvm/views/activities/MainActivity.java
+++ app/src/main/java/com/krp/mvvm/views/activities/MainActivity.java
@@ -1,13 +1,16 @@
-package com.krp.mvvm;
+package com.krp.mvvm.views.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this,DashboardActivity.class));
+        finish();
     }
 }
